f093c6d710ccae109e6df5fa699df89d3fd6cb7e
Josie Rose <113395782+jrose86@users.noreply.github.com>
2024-05-04 20:46:05 -0400

    Create testbool.C

---
+++ testbool.C
@@ -0,0 +1,12 @@
+// josie 21 jan 2024
+// test of command line input boolean
+
+void testbool(int off){
+
+  if (off == 1){ cout << "off true" << endl;}
+
+  if (off == 0){ cout << "off false" << endl;}
+
+}
+
+// ok to get this to run, you must execute the macro as: root 'testbool.C(1)' or from within interactive root terminal
